eb5bb6f104ba82c6a4b6fc9cef962326a2f7c035
mihneabuz <mihneabuzatu88@gmail.com>
2022-05-06 17:48:43 +0300

    whoops forgot some debug prints

--- src/graphql/services/gameEngineService.ts
+++ src/graphql/services/gameEngineService.ts
@@ -3,9 +3,7 @@ import axios from 'axios';
 const serviceUrl = 'http://game-engine-service:3000/engine';
 
 async function suggetMove(fen: string) {
-  console.log(fen);
   const res = await axios.post(`${serviceUrl}/move/suggest`, { fen });
-  console.log(res);
   return res.data;
 }
 
